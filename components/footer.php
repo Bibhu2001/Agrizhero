5459945140a2004de007bd96d2ed156c652f587c
Bibhukalyan Mohanty <bibhukalyan@lpu.in>
2024-04-03 14:52:45 +0530

    start

---
+++ components/footer.php
@@ -0,0 +1,74 @@
+<!--Footer Start-->
+<footer class="footer">
+    <div class="footer-top wf100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <h4>About AgriZhero</h4>
+                        <p>We are a team of experts who have learnt more from the ground than the classrooms. With an expertise in education, media, branding, marketing, banking, agri inputs, govt linkages, we as a team are a holistic environment which is build to uplift the Indian Farming Community by developing “farmpreneurs” across the country </p>
+                        <a href="about.php" class="lm">About us</a>
+                    </div>
+                    <!--Footer Widget End-->
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <h4>Our Promises</h4>
+                        <ul class="quick-links">
+                            <li><a href="#">Building a Strong Identity for Your Product or Content</a></li>
+                            <li><a href="#">Engaging Your Audience Through Narrative</a></li>
+                            <li><a href="#">Reaching Your Target Market</a></li>
+                            <li><a href="#">Leveraging Key Partnerships for Brand Growth</a></li>
+                            <li><a href="#">SEO, PPC, and Display Ads</a></li>
+                            <li><a href="#">Nurturing Relationships with Customers </a></li>
+                        </ul>
+                    </div>
+                    <!--Footer Widget End-->
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <h4>Latest Posts</h4>
+                        <ul class="lastest-products">
+                            <li> <img src="images/flp1.jpg" alt=""> <strong><a href="#">How you can keep alive wild
+                                        animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September,
+                                    2018</span> </li>
+                            <li> <img src="images/flp2.jpg" alt=""> <strong><a href="#">Eliminate your plastic bottle
+                                        pollution & keep...</a></strong> <span class="pdate"><i>Posted:</i> 29
+                                    September, 2018</span> </li>
+                            <li> <img src="images/flp3.jpg" alt=""> <strong><a href="#">How you can keep alive wild
+                                        animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September,
+                                    2018</span> </li>
+                        </ul>
+                    </div>
+                    <!--Footer Widget End-->
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!-- social icons -->
+                    <div class="footer-widget">
+                        <h4>Get In Touch</h4>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyr wf100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4"> <img src="../images/new.png" alt=""> </div>
+                    <div class="col-md-8 col-sm-8">
+                        <p><a target="_blank" href="#">&copy; Copyright 2024 Agrizhero
+                                All Rights Reserved</a></p>
+                        </a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+<!--Footer End-->
---
+++ components/nav.php
@@ -0,0 +1,28 @@
+  <!--Header Start-->
+  <header class="header-style-2">
+            <nav class="navbar navbar-expand-lg">
+               <a class="navbar-brand" href="index.php"><img src="../images/new.png" alt=""></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="./index.php" aria-haspopup="true" aria-expanded="false"> Home </a>
+                     </li>
+                     <li class="nav-item"> <a class="nav-link" href="./about.php">About</a> </li>
+                     <li class="nav-item"><a class="nav-link dropdown-toggle" href="./events.php" > Events </a> </li>
+                     <li class="nav-item"><a class="nav-link dropdown-toggle" href="./blogs.php" > Blogs </a></li>
+                     <li class="nav-item"><a class="nav-link dropdown-toggle" href="./team.php"  > Team </a></li>
+                     <li class="nav-item"><a class="nav-link dropdown-toggle" href="./contact.php"> Contact </a></li>
+                  </ul>
+                  
+               </div>
+            </nav>
+         </header>
+         <div id="search">
+            <button type="button" class="close">×</button>
+            <form class="search-overlay-form">
+               <input type="search" value="" placeholder="type keyword(s) here" />
+               <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+            </form>
+         </div>
+         <!--Header End--> 
